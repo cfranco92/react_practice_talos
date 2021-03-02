1824aa7d7ae2a1d0aaa63cf8d99da5ac1cdf8ca2
Cristian Franco Bedoya <cfrancobedoya@gmail.com>
2021-03-02 14:19:16 -0500

    Renaming functions and splitting modal View.

--- src/components/ModalView/ModalView.js
+++ src/components/ModalView/ModalView.js
@@ -1,9 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import styles from './ModalView.module.css'
-import { Modal, Image, Row, Col, Table, Button } from 'react-bootstrap';
-import ChartTool from '../ChartTool'
-import DobleChartTool from '../DobleChartTool'
+import ModalOnePokemon from './ModalOnePokemon'
+import ModalTwoPokemons from './ModalTwoPokemons'
 import { setShow } from '../../redux/actions/modalViewActions'
 import { cleanSelectedPokemons } from '../../redux/actions/pokemonsActions'
 import { setShowToast } from '../../redux/actions/pokemonsActions'
@@ -16,9 +14,7 @@ const ModalView = ({
   setShowToast,
   showToast
 }) => {
-  const clases = styles
-
-  const handleClick = (e) => {
+  const cleanPokemonArray = () => {
     setShow(modalViewState)
     cleanSelectedPokemons()
     if (showToast) {
@@ -26,7 +22,7 @@ const ModalView = ({
     }
   }
 
-  const handleCompareClick = (e) => {
+  const keepPokemon = () => {
     setShow(modalViewState)
     setShowToast()
   }
@@ -35,216 +31,21 @@ const ModalView = ({
     <>
       {showToast && selectedPokemons.length > 1
         ?
-        // VIEW FOR 2 POKEMONS
-        <Modal
-          size="m"
-          show={modalViewState}
-          onHide={handleClick}
-          aria-labelledby="example-modal-sizes-title-lg"
-        >
-          <Modal.Header closeButton>
-            <Modal.Title id="example-modal-sizes-title-lg">
-              <div>
-                {String(selectedPokemons[0].name).toUpperCase()} VS. {String(selectedPokemons[1].name).toUpperCase()}
-              </div>
-            </Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <div >
-              <div>
-                <Row>
-                  <Col xs={12} md={12}>
-                    <Row>
-                      <Col xs={6} md={6}>
-                        <Image variant="top" src={`${selectedPokemons[0].image}`} className={clases.image} fluid />
-                      </Col>
-                      <Col xs={6} md={6}>
-                        <Image variant="top" src={`${selectedPokemons[1].image}`} className={clases.image} fluid />
-                      </Col>
-                    </Row>
-                  </Col>
-                  <Col xs={12} md={12}>
-                    <hr />
-                    <Table borderless size='sm' className={clases.tableInformation}>
-                      <thead>
-                        <tr>
-                          <td>{selectedPokemons[0].height}m</td>
-                          <th>Height</th>
-                          <td>{selectedPokemons[1].height}m</td>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        <tr>
-                          <td>{selectedPokemons[0].weight}kg</td>
-                          <th>Weight</th>
-                          <td>{selectedPokemons[1].weight}kg</td>
-                        </tr>
-                        <tr>
-                          <td>{selectedPokemons[0].gender}</td>
-                          <th>Gender</th>
-                          <td>{selectedPokemons[1].gender}</td>
-                        </tr>
-                        <tr>
-                          <td>
-                            {
-                              selectedPokemons[0].abilities.map((abilities) => {
-                                return (
-                                  <div key={abilities.ability.name}>
-                                    {abilities.ability.name}
-                                  </div>
-                                )
-                              })
-                            }
-                          </td>
-                          <th>Abilities</th>
-                          <td>
-                            {
-                              selectedPokemons[1].abilities.map((abilities) => {
-                                return (
-                                  <div key={abilities.ability.name}>
-                                    {abilities.ability.name}
-                                  </div>
-                                )
-                              })
-                            }
-                          </td>
-                        </tr>
-                      </tbody>
-                    </Table>
-                  </Col>
-                </Row>
-                <hr />
-                <h1 className={clases.name}>Stats</h1>
-                <DobleChartTool
-                  stats={selectedPokemons[0].stats.map((stat) => {
-                    return stat.stat.name
-                  })}
-                  bases={selectedPokemons[0].stats.map((stat) => {
-                    return stat.base_stat
-                  })}
-                  name={selectedPokemons[0].name}
-                  color={selectedPokemons[0].color.name}
-                  stats2={selectedPokemons[1].stats.map((stat) => {
-                    return stat.stat.name
-                  })}
-                  bases2={selectedPokemons[1].stats.map((stat) => {
-                    return stat.base_stat
-                  })}
-                  name2={selectedPokemons[1].name}
-                  color2={selectedPokemons[1].color.name}
-                />
-              </div>
-            </div>
-          </Modal.Body>
-        </Modal>
+        <ModalTwoPokemons
+          selectedPokemons={selectedPokemons}
+          modalViewState={modalViewState}
+          cleanPokemonArray={cleanPokemonArray}
+        />
 
         :
-        // VIEW FOR 1 POKEMON
-        <Modal
-          size="m"
-          show={modalViewState}
-          onHide={handleClick}
-          aria-labelledby="example-modal-sizes-title-lg"
-        >
-          <Modal.Header closeButton>
-            <Modal.Title id="example-modal-sizes-title-lg">
-              {selectedPokemons.map((pokemon, index = 1) => {
-                return (
-                  <div key={index + Math.random()}>
-                    {String(pokemon.name).toUpperCase()}
-                    <Button className={clases.modalButton} onClick={handleCompareClick} variant="secondary">Compare to...</Button>
-                  </div>
-                )
-              })}
-            </Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            {selectedPokemons.map((pokemon, index = 1) => {
-              return (
-                <div key={index + Math.random()}>
-                  <div>
-                    <Row>
-                      <Col xs={4} md={4}>
-                        <Image variant="top" src={`${pokemon.image}`} className={clases.image} fluid />
-                      </Col>
-                      <Col xs={8} md={8}>
-                        {pokemon.flavor_text_entries[1].flavor_text}
-                        <hr />
-                        <Table borderless size='sm'>
-                          <thead>
-                            <tr>
-                              <th>Height</th>
-                              <th>Weight</th>
-                              <th>Gender</th>
-                            </tr>
-                          </thead>
-                          <tbody>
-                            <tr>
-                              <td>{pokemon.height}m</td>
-                              <td>{pokemon.weight}kg</td>
-                              <td>{selectedPokemons[0].gender}</td>
-                            </tr>
-                          </tbody>
-                        </Table>
-                        <Table borderless size='sm'>
-                          <thead>
-                            <tr>
-                              <th>Abilities</th>
-                              <th>Type</th>
-                            </tr>
-                          </thead>
-                          <tbody>
-                            <tr>
-                              <td>
-                                <ul>
-                                  {
-                                    pokemon.abilities.map((abilities) => {
-                                      return (
-                                        <li key={abilities.ability.name}>
-                                          {abilities.ability.name}
-                                        </li>
-                                      )
-                                    })
-                                  }
-                                </ul>
-                              </td>
-                              <td>
-                                <ul>
-                                  {
-                                    pokemon.types.map((types) => {
-                                      return (
-                                        <li key={types.type.name}>
-                                          {types.type.name}
-                                        </li>
-                                      )
-                                    })
-                                  }
-                                </ul>
-                              </td>
-                            </tr>
-                          </tbody>
-                        </Table>
-                      </Col>
-                    </Row>
-                    <hr />
-                    <h1 className={clases.name}>Stats</h1>
-                    <ChartTool
-                      stats={pokemon.stats.map((stat) => {
-                        return stat.stat.name
-                      })}
-                      bases={pokemon.stats.map((stat) => {
-                        return stat.base_stat
-                      })}
-                      name={pokemon.name}
-                      color={pokemon.color.name}
-                    />
-                  </div>
+        <ModalOnePokemon
+          selectedPokemons={selectedPokemons}
+          modalViewState={modalViewState}
+          cleanPokemonArray={cleanPokemonArray}
+          keepPokemon={keepPokemon}
+        />
 
-                </div>
-              )
-            })}
-          </Modal.Body>
-        </Modal>
+
       }
     </>
   );
